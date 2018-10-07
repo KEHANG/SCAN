96963ee67b61c1e761eab6e4b8a78218bf4a16fb
hericopter <947414702@qq.com>
2018-10-07 18:28:27 +0800

    DAE

--- scripts/DAE.sh
+++ scripts/DAE.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-python main.py --SCAN --phase DAE --dataset celeba --seed 3 --lr 1e-3 --batch_size 100 --max_iter 2e5 --env_name DAE
+python main.py --SCAN --phase DAE --dataset celeba --seed 3 --lr 1e-3 --batch_size 100 --max_iter 3e5 --env_name DAE
