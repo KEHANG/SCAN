7e85828a53a4d04710b303a0574f686822ca7615
hericopter <947414702@qq.com>
2018-10-07 16:08:54 +0800

    debug

--- scripts/original_beta_VAE.sh
+++ scripts/original_beta_VAE.sh
@@ -2,4 +2,4 @@
 
 python main.py --dataset celeba --seed 1 --lr 1e-4 --beta1 0.9 --beta2 0.999 \
     --objective H --model H --batch_size 64 --z_dim 10 --max_iter 1.5e6 \
-    --beta 10 --env_name trial --display_save_step 10000
+    --beta 10 --env_name beta_VAE --display_save_step 10000
