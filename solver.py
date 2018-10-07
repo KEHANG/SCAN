b90704175738b9679ecbc1a611a51736d24231ca
hericopter <947414702@qq.com>
2018-10-07 10:04:35 +0800

    debug

--- solver.py
+++ solver.py
@@ -185,13 +185,15 @@ class super_beta_VAE(Solver):
         klds = torch.cat([dim_wise_klds, mean_klds, total_klds], 1).cpu()
         iters = torch.Tensor(self.gather.data['iter'])
 
-        def update_win(Y, win, legend=[], title=''):
-            if win is None:
-                return self.vis.line(X=iters, Y=Y, env=self.args.env_name+'_lines',
-                                     opts=dict( width=400, height=400, legend=legend, xlabel='iteration', title=title,))
+        def update_win(Y, win, legend=None, title=''):
+            if legend is None:
+                opts = dict( width=400, height=400, xlabel='iteration', title=title,)
             else:
-                return self.vis.line(X=iters, Y=Y, env=self.args.env_name+'_lines', win=win, update='remove',
-                                     opts=dict( width=400, height=400, legend=legend, xlabel='iteration', title=title,))
+                opts = dict( width=400, height=400, legend=legend, xlabel='iteration', title=title,))
+            if win is None:
+                return self.vis.line(X=iters, Y=Y, env=self.args.env_name+'_lines', opts=opts)
+            else:
+                return self.vis.line(X=iters, Y=Y, env=self.args.env_name+'_lines', win=win, update='remove', opts=opts)
 
         legend = []
         for z_j in range(self.args.z_dim):
@@ -199,10 +201,10 @@ class super_beta_VAE(Solver):
         legend.append('mean')
         legend.append('total')
 
-        self.win_recon = update_win(recon_losses, self.win_recon, ['reconstruction loss'], 'reconstruction loss')
-        self.win_kld = update_win(klds, self.win_kld, legend, 'kl divergence')
-        self.win_mu = update_win(mus, self.win_mu, legend[self.args.z_dim], 'posterior mean')
-        self.win_var = update_win(variances, self.win_var, legend[self.args.z_dim], 'posterior variance')
+        self.win_recon = update_win(recon_losses, self.win_recon, title='reconstruction loss')
+        self.win_kld = update_win(klds, self.win_kld, legend, title='kl divergence')
+        self.win_mu = update_win(mus, self.win_mu, legend[self.args.z_dim], title='posterior mean')
+        self.win_var = update_win(variances, self.win_var, legend[self.args.z_dim], title='posterior variance')
 
         self.net_mode(train=True)
 
