767e3e9ebb1ae79d12ba18c7037e0402f3e3bf08
hericopter <947414702@qq.com>
2018-10-08 16:00:30 +0800

    SCAN_data

--- dataset.py
+++ dataset.py
@@ -46,8 +46,8 @@ class CustomMixDataset(Dataset):
         for line in lines:
             words = [word for word in line.split(' ')[1:] if word!='' and word!='\n']
             vector = list(map(lambda x: (1 + int(x)) // 2, words))
-            print(vector)
             vector = np.array(vector).resize([1, self.n_key])
+            print(vector.size())
             attr_tensor = vector if attr_tensor is None else np.concatenate([attr_tensor, vector])
 
         return attr_tensor
