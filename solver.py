36eb58b4b471d7359ce31d1806c46fd84f1faf00
hericopter <947414702@qq.com>
2018-10-08 20:32:07 +0800

    SCAN

--- solver.py
+++ solver.py
@@ -489,7 +489,7 @@ class SCAN(Solver):
                 sym_text = sym_text + '[{0}: {1:.3f}]\n'.format(self.keys[index], y_x[index])
             drawer.text((100, 20), sym_text)
 
-            images.append(transforms.ToTensor()(board))
+            images.append(transforms.ToTensor()(board).data)
         self.vis.images(images, env=self.env_name+'_img2sym',
                         opts=dict(title='iter:{}'.format(self.global_iter)), nrow=int(math.sqrt(num_img2sym)))
 
