d28e2d8806579f259a850b1f9c071f8f42c7f17a
hericopter <947414702@qq.com>
2018-10-08 20:01:01 +0800

    SCAN

--- dataset.py
+++ dataset.py
@@ -56,6 +56,7 @@ class CustomMixDataset(Dataset):
             vector.resize([1, self.n_key])
             attr_tensor.append(vector)
         attr_tensor = np.concatenate(attr_tensor)
+        print(self.keys)
         pbar.write('[Dataset Loading Finished]')
         pbar.close()
 
