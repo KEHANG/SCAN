1f41d090cd2ddd1f532d9b5d3dd7dbef7580a4a4
hericopter <947414702@qq.com>
2018-10-09 09:34:17 +0800

    readme

--- scripts/beta_VAE.sh
+++ scripts/beta_VAE.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-python main.py --dataset celeba --root_dir /data/hc/SCAN/\
+python main.py --dataset celeba\
     --SCAN --phase beta_VAE\
     --seed 7 --lr 1e-4 --batch_size 100 --max_iter 2e6 --beta 53\
     --DAE_env_name DAE --DAE_z_dim 100\
