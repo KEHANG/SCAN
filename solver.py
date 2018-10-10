d71bedde5c3fe40697b0a826458824f77f4760db
hericopter <947414702@qq.com>
2018-10-10 15:30:45 +0800

    SCAN

--- solver.py
+++ solver.py
@@ -509,7 +509,7 @@ class SCAN(Solver):
         images = []
         for i in range(self.n_key):
             random_ys = np.random.randint(2, size=[num_sym2img, self.nc])
-            random_ys[:, i] = 1
+            random_ys[:, i] = 2
             random_ys = self.tensor(random_ys)
             image_subset = self.DAE_net(self.beta_VAE_net._decode(self.net._encode(random_ys))).cpu().data
             nrow = int(math.sqrt(num_sym2img))
